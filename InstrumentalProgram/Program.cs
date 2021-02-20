1a3385f86488fc249a89e16118ab782b90103ed5
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-20 15:13:28 +0600

    Добавьте файлы проекта.

---
+++ InstrumentalProgram/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace InstrumentalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileInputName = @"OneExeProgram\Libraries\AutoMapper 1.0 RTW\AutoMapper.dll";
+            var assembly = File.ReadAllBytes(fileInputName);
+
+            var fileOutputName = @"OneExeProgram\Libraries\AutoMapper 1.0 RTW\AutoMapper.dll.deflated";
+            using (var file = File.Open(fileOutputName, FileMode.Create))
+            using (var stream = new DeflateStream(file, CompressionMode.Compress))
+            using (var writer = new BinaryWriter(stream))
+            {
+                writer.Write(assembly);
+            }
+        }
+        private static Assembly AppDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.Contains("AutoMapper"))
+            {
+                Console.WriteLine("Resolving assembly: {0}", args.Name);
+
+                // Загрузка запакованной сборки из ресурсов, ее распаковка и подстановка
+                using (var resource = new MemoryStream(System.Resources.AutoMapper_dll))
+                using (var deflated = new DeflateStream(resource, CompressionMode.Decompress))
+                using (var reader = new BinaryReader(deflated))
+                {
+                    var one_megabyte = 1024 * 1024;
+                    var buffer = reader.ReadBytes(one_megabyte);
+                    return Assembly.Load(buffer);
+                }
+            }
+
+            return null;
+        }
+    }
+}
